d99c971b90aa7e96fd3486c06458896de7264225
David Freitas <davidfreitas@MacBook-Pro-de-David.local>
2022-03-27 12:11:44 -0300

    configuracao inicial do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,45 @@
 <template>
   <ion-app>
-    <ion-router-outlet />
+    <ion-menu menuId="main-menu" content-id="main" side="start" type="overlay">
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>Task Manager</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content>
+        <ion-list>
+          <ion-menu-toggle>
+            <ion-item router-link="/">
+              <ion-icon name="save" slot="start"></ion-icon>
+              <ion-label>Listas Salvas</ion-label>
+            </ion-item>
+          </ion-menu-toggle>
+          <ion-menu-toggle>
+            <ion-item router-link="/">
+              <ion-icon name="heart" slot="start"></ion-icon>
+              <ion-label>Favoritas</ion-label>
+            </ion-item>
+          </ion-menu-toggle>
+          <ion-menu-toggle>
+            <ion-item router-link="/">
+              <ion-icon name="trash" slot="start"></ion-icon>
+              <ion-label>Lixeira</ion-label>
+            </ion-item>
+          </ion-menu-toggle>
+        </ion-list>
+      </ion-content>
+      <ion-footer class="ion-padding">
+        Task Manager v1.0
+      </ion-footer>
+    </ion-menu>
+    <ion-router-outlet id="main"></ion-router-outlet>
   </ion-app>
 </template>
 
-<script lang="ts">
+<script>
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
-import { defineComponent } from 'vue';
 
-export default defineComponent({
+export default ({
   name: 'App',
   components: {
     IonApp,
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,30 +2,25 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Task Manager</ion-title>
+        <ion-buttons slot="start">
+          <ion-menu-button menu="main-menu"></ion-menu-button>
+        </ion-buttons>
       </ion-toolbar>
     </ion-header>
     
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        
       </div>
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
+<script>
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
 
-export default defineComponent({
+export default ({
   name: 'HomePage',
   components: {
     IonContent,
